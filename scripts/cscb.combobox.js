877dffadb1b82adf47603542d87b11603f861ec4
cstephe <cstephe@gmail.com>
2014-01-24 14:57:41 -0500

    More project setup and finetuning of text-select-combo Will start feature branching once branching things get stable

--- scripts/cscb.combobox.js
+++ scripts/cscb.combobox.js
@@ -3,8 +3,9 @@
 'use strict';
 (function(window, angular, undefined) {
     angular.module('cscb.comboboxes', [])
-.directive('cscbTextSelectCombo', ['_',
-    function(_) {
+.directive('cscbTextSelectCombo', ['$document' ,'_', 'CSCBService',
+    function($document, _, CSCBService) {
+        var settings = CSCBService.getSettings();
         var directive = {
             restrict : 'E',
             scope : {
@@ -13,16 +14,50 @@
             }
         };
         var linkFn = function(scope){
+            var onClickAnyWhereElse = function(event){
+                var originalTargetScope = angular.element(event.originalEvent.srcElement).scope() || {};
+                if(originalTargetScope.$id !== scope.$id)
+                {
+                    scope.popupVisible=false;
+                    scope.$apply(scope.popupVisible);
+                    $document.unbind('click', onClickAnyWhereElse);
+                }
+            };
             scope.popUp = function(){
                 scope.popupVisible = !scope.popupVisible;
+                // Close all instances when user clicks elsewhere
+                $document.bind('click', onClickAnyWhereElse);
             };
             scope.selectItem = function(item){
                 scope.comboData = item;
+                scope.popupVisible = false;
             };
             scope.popupVisible=false;
         };
         directive.link = linkFn;
-        directive.templateUrl = "templates/text-select-combo.html";
+        directive.templateUrl = settings.templatePath + "text-select-combo.html";
         return directive;
-}])
+}])// Service to allow host pages to change settings for all instances (in their module.run function)
+        .factory('CSCBService', function () {
+
+            var defaultSettings = {
+                "templatePath": "templates"
+            };
+
+            return {
+                getSettings: function () {
+                    // Add trailing "/" to template path if not present
+                    var len = defaultSettings.templatePath.length;
+                    if (len > 0 && defaultSettings.templatePath.substr(len - 1, 1) !== "/") {
+                        defaultSettings.templatePath += "/";
+                    }
+                    return angular.copy(defaultSettings);
+                },
+                updateSetting: function (settingName, value) {
+                    if (defaultSettings.hasOwnProperty(settingName)) {
+                        defaultSettings[settingName] = value;
+                    }
+                }
+            };
+        });
 })(window, window.angular);
